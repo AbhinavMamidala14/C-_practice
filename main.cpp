1f18c19d3a09eda8a3f20045478fc34198bd911b
Shruthi Venkateshan <shruthivenkateshan@macbook.local>
2024-11-30 19:54:06 -0800

    committing code for sum of two numbers

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter two numbers for which you want to calculate sum";
+    cin>>a>>b;
+    cout<<"sum of a and b is : "<<a+b;
+}
